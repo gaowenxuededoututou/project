182e98f4f68a0f316bac3f2d002279cc4fc1f8fe
xixifusili07 <xixifusili07@sina.com>
2023-03-05 23:21:04 +0800

    css

---
+++ web-project/scripts/main.js
@@ -0,0 +1,18 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = '你好，世界';
+let myVariable;
+myVariable = '李雷';
+/*
+这里的所有内容
+都是注释。
+*/
+let myVariable11 = document.querySelector('h1');
+alert('hello!');
+function multiply(num1, num2) {
+    let result = num1 - num2;
+    return result;
+  }
+  document.querySelector("h1").addEventListener("click", () => {
+    alert("lalal。");
+  });
+  
